ce78902f519c3fa7a87c624c77371048641117c6
danielmvalentine <valentine.da@northeastern.edu>
2025-09-08 20:30:39 -0400

    Up to buttons

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -148,12 +148,57 @@ export default function Lab1() {
         </table>
       </div>
 
+      <div id="wd-images">
+        <h4>Image tag</h4>
+        Loading an image from the internet: <br />
+        <img id="wd-starship" width="400px"   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+        <br />
+        Loading a local image:
+        <br />
+        <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" /></div>
+
         </div>
 
         <div id="wd-h-tag"> 
-            <h4>Lab 1.4</h4>
+            <h4>Lab 1.4 - Web Forms</h4>
         </div>
+
+        <div id="wd-forms">
+            <h4>Form Elements</h4>
+            <form id="wd-text-fields">
+                <h5>Text Fields</h5>
+                <label htmlFor="wd-text-fields-username">Username:</label>
+                <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+                <label htmlFor="wd-text-fields-password">Password:</label>
+                <input type="password" value="123@#$asd" id="wd-text-fields-password" />
+                <br />
+                <label htmlFor="wd-text-fields-first-name">First name:</label>
+                <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+                <label htmlFor="wd-text-fields-last-name">Last name:</label>
+                <input type="text" value="Doe"
+                    value="Wonderland"
+                    title="The last name"
+                    id="wd-text-fields-last-name" />
+                {/* copy rest of form elements here  */}
+            </form>
+        </div>
+
+
+        <h5>Text boxes</h5>
+        <label>Biography:</label><br/>
+        <textarea id="wd-textarea" cols={30} rows={10}>
+        Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.</textarea>
+
 
       </div>
     </div>
+
+    
   );}
+
