936d952064965f19ca6df31da50bff43d13e6ddc
danielmvalentine <valentine.da@northeastern.edu>
2025-10-03 18:08:27 -0400

    doneeee

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
        <NavLink href="/" as={Link}>Kambaz</NavLink>
      </NavItem>
      <NavItem>
-      <NavLink ID="wd-github">https://github.com/danielmvalentine/kambaz-next-js-cs4550-fa25</NavLink>
+      <NavLink href="https://github.com/danielmvalentine/kambaz-next-js-cs4550-fa25">wd-github</NavLink>
      </NavItem>
      <NavItem>
        <NavLink href="https://github.com/jannunzi">My GitHub</NavLink>
