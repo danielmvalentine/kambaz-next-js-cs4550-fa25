fd7eec33fe78fb62ef2e2e9ba67f1d8948bd0cdd
danielmvalentine <valentine.da@northeastern.edu>
2025-11-08 11:38:10 -0500

    added rudimentary add course feature:

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,12 +1,35 @@
+"use client"
+import { useState } from "react";
 import Link from "next/link";
 import * as db from "../Database";
+import { v4 as uuidv4 } from "uuid";
+// import { FormControl } from '@mui/material';
 
 export default function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/NEU.png", description: "New Description"
+  });
+
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: uuidv4() };
+    setCourses([...courses, newCourse ]);
+  };
+
   
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1>
+      <h5>New Course
+          <button className="btn btn-primary float-end"
+                  id="wd-add-new-course-click"
+                  onClick={addNewCourse} > Add </button>
+      </h5>
+      <br />
+      {/* <FormControl value={course.name} className="mb-2" />
+      <FormControl value={course.description} rows={3}/><hr /> */}
       <hr />
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
       <hr />
