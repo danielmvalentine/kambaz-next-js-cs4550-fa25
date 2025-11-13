8eeda6f35acf6afd682f6b70aacb36412c44a192
danielmvalentine <valentine.da@northeastern.edu>
2025-11-13 17:42:51 -0500

    Finished with the Lab4 link and setup

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -19,6 +19,13 @@ export default function TOC() {
        <NavLink href="/Labs/Lab3" as={Link} className={`nav-link ${pathname.endsWith("Lab3") ? "active" : ""}`}>
          Lab 3 </NavLink> </NavItem>
      <NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab4" as={Link} className={`nav-link ${pathname.endsWith("Lab4") ? "active" : ""}`}>
+        Lab 4 </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab5" as={Link} className={`nav-link ${pathname.endsWith("Lab5") ? "active" : ""}`}>
+        Lab 5 </NavLink> </NavItem>
+      
        <NavLink href="/" as={Link}>Kambaz</NavLink>
      </NavItem>
      <NavItem>
--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -23,6 +23,11 @@ export default function Labs() {
         </Link>
        </li>
        <li>
+        <Link href="/Labs/Lab5" id="wd-lab5-link">
+          Lab 5: Implementing RESTful Web APIs with Express.js
+        </Link>
+       </li>
+       <li>
        <Link href="/" id="wd-lab3-link">
          Kambaz </Link>
         </li>
