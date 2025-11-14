46b828756af395a77d96a26f6a63bd147842df91
danielmvalentine <valentine.da@northeastern.edu>
2025-11-13 19:44:47 -0500

    up to 5 2 5

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -1,6 +1,8 @@
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export default function Lab5() {
     return (
@@ -14,6 +16,8 @@ export default function Lab5() {
         <EnvironmentVariables />
         <PathParameters />
         <QueryParameters />
+        <WorkingWithObjects />
+        <WorkingWithArrays />
       </div>
   );}
   
