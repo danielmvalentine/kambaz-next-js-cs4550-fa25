fd201bbb368512d21eaccc89b8f9cde16518e3eb
danielmvalentine <valentine.da@northeastern.edu>
2025-12-02 19:16:33 -0500

    Fixed cookies

--- app/(Kambaz)/Account/client.ts
+++ app/(Kambaz)/Account/client.ts
@@ -1,28 +1,29 @@
 import axios from "axios";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER || "http://localhost:4000";
 const USERS_API = `${HTTP_SERVER}/api/users`;
+
 const axiosWithCredentials = axios.create({
   baseURL: USERS_API,
   withCredentials: true,
 });
 
 export const findUsersByRole = async (role: string) => {
-  const response = await axios.get(`${USERS_API}?role=${role}`);
+  const response = await axiosWithCredentials.get(`?role=${role}`);
   return response.data;
 };
 
-export const findUsersByPartialName = async (name: string) => {  // ADD THIS
-  const response = await axios.get(`${USERS_API}?name=${name}`);
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axiosWithCredentials.get(`?name=${name}`);
   return response.data;
 };
 
 export const findAllUsers = async () => {
-  const response = await axios.get(USERS_API);
+  const response = await axiosWithCredentials.get('/');
   return response.data;
 };
 
 export const findUserById = async (id: string) => {
-  const response = await axios.get(`${USERS_API}/${id}`);
+  const response = await axiosWithCredentials.get(`/${id}`);
   return response.data;
 };
 
@@ -42,12 +43,12 @@ export const profile = async () => {
 };
 
 export const updateUser = async (user: any) => {
-  const response = await axiosWithCredentials.put("/profile", user);  // Changed to /profile
+  const response = await axiosWithCredentials.put("/profile", user);
   return response.data;
 };
 
 export const updateUserById = async (userId: string, user: any) => {
-  const response = await axios.put(`${USERS_API}/${userId}`, user);
+  const response = await axiosWithCredentials.put(`/${userId}`, user);
   return response.data;
 };
 
@@ -57,11 +58,11 @@ export const signout = async () => {
 };
 
 export const deleteUser = async (userId: string) => {
-  const response = await axios.delete( `${USERS_API}/${userId}` );
+  const response = await axiosWithCredentials.delete(`/${userId}`);
   return response.data;
 };
 
 export const createUser = async (user: any) => {
-  const response = await axios.post(`${USERS_API}`, user);
+  const response = await axiosWithCredentials.post('/', user);
   return response.data;
 };
